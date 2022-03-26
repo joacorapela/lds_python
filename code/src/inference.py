54274e5fdeeebb286fb7d1ab4e16fa41d60100c3
Joaquin Rapela <joacorapela@gmail.com>
2022-03-26 15:58:58 +0000

    Changed matmul by @ and there stylistic changes

--- code/src/inference.py
+++ code/src/inference.py
@@ -46,40 +46,36 @@ def filterLDS_SS_withMissingValues(y, B, Q, m0, V0, Z, R):
     Sn = np.empty(shape=[P, P, N])
 
     # k==0
-    xnn1[:, 0, 0] = np.dot(B, m0)
-    Vnn1[:, :, 0] = np.matmul(B, np.matmul(V0, np.transpose(B)))+Q
-    Stmp = np.matmul(Z, np.matmul(Vnn1[:, :, 0], np.transpose(Z)))+R
-    Sn[:, :, 0] = (Stmp+np.transpose(Stmp))/2
+    xnn1[:, 0, 0] = (B @ m0).squeeze()
+    Vnn1[:, :, 0] = B @ V0 @ B.T + Q
+    Stmp = Z @ Vnn1[:, :, 0] @ Z.T + R
+    Sn[:, :, 0] = (Stmp + np.transpose(Stmp)) / 2
     Sinv = np.linalg.inv(Sn[:, :, 0])
-    K = np.matmul(Vnn1[:, :, 0], np.matmul(np.transpose(Z), Sinv))
-    innov[:, 0, 0] = y[:, 0]-(np.matmul(Z, xnn1[:, :, 0]).squeeze())
-    xnn[:, :, 0] = xnn1[:, :, 0]+np.matmul(K, innov[:, :, 0])
-    Vnn[:, :, 0] = Vnn1[:, :, 0]-np.matmul(K, np.matmul(Z, Vnn1[:, :, 0]))
-    logLike = -N*P*np.log(2*np.pi)-np.linalg.slogdet(Sn[:, :, 0])[1] - \
-        np.matmul(np.transpose(innov[:, :, 0]),
-                  np.matmul(Sinv, innov[:, :, 0]))
+    K = Vnn1[:, :, 0] @ Z.T @ Sinv
+    innov[:, 0, 0] = y[:, 0] - (Z @  xnn1[:, :, 0]).squeeze()
+    xnn[:, :, 0] = xnn1[:, :, 0] + K @ innov[:, :, 0]
+    Vnn[:, :, 0] = Vnn1[:, :, 0] - K @ Z @ Vnn1[:, :, 0]
+    logLike = -N*P*np.log(2*np.pi) - np.linalg.slogdet(Sn[:, :, 0])[1] - \
+        innov[:, :, 0].T @ Sinv @ innov[:, :, 0]
 
     # k>1
     for k in range(1, N):
-        xnn1[:, :, k] = np.matmul(B, xnn[:, :, k-1])
-        Vnn1[:, :, k] = np.matmul(B, np.matmul(Vnn[:, :, k-1],
-                                               np.transpose(B))) + Q
+        xnn1[:, :, k] = B @ xnn[:, :, k-1]
+        Vnn1[:, :, k] = B @ Vnn[:, :, k-1] @ B.T + Q
         if(np.any(np.isnan(y[:, k]))):
             xnn[:, :, k] = xnn1[:, :, k]
             Vnn[:, :, k] = Vnn1[:, :, k]
         else:
-            Stmp = np.matmul(Z, np.matmul(Vnn1[:, :, k], np.transpose(Z))) + R
-            Sn[:, :, k] = (Stmp+np.transpose(Stmp))/2
+            Stmp = Z @ Vnn1[:, :, k] @ Z.T + R
+            Sn[:, :, k] = (Stmp + Stmp.T)/2
             Sinv = np.linalg.inv(Sn[:, :, k])
-            K = np.matmul(Vnn1[:, :, k], np.matmul(np.transpose(Z), Sinv))
-            innov[:, 0, k] = y[:, k]-(np.matmul(Z, xnn1[:, :, k]).squeeze())
-            xnn[:, :, k] = xnn1[:, :, k] + np.matmul(K, innov[:, :, k])
-            Vnn[:, :, k] = Vnn1[:, :, k] - \
-                np.matmul(K, np.matmul(Z, Vnn1[:, :, k]))
+            K = Vnn1[:, :, k] @ Z.T @ Sinv
+            innov[:, 0, k] = y[:, k] - (Z @ xnn1[:, :, k]).squeeze()
+            xnn[:, :, k] = xnn1[:, :, k] + K @ innov[:, :, k]
+            Vnn[:, :, k] = Vnn1[:, :, k] - K @ Z @ Vnn1[:, :, k]
         logLike = logLike-np.linalg.slogdet(Sn[:, :, k])[1] -\
-            np.matmul(np.transpose(innov[:, :, k]),
-                      np.matmul(Sinv, innov[:, :, k]))
-    logLike = 0.5*logLike
+            innov[:, :, k].T @ Sinv @ innov[:, :, k]
+    logLike = 0.5 * logLike
     answer = {"xnn1": xnn1, "Vnn1": Vnn1, "xnn": xnn, "Vnn": Vnn,
               "innov": innov, "KN": K, "Sn": Sn, "logLike": logLike}
     return answer
@@ -121,21 +117,16 @@ def smoothLDS_SS(B, xnn, Vnn, xnn1, Vnn1, m0, V0):
     xnN[:, :, -1] = xnn[:, :, -1]
     VnN[:, :, -1] = Vnn[:, :, -1]
     for n in reversed(range(1, N)):
-        Jn[:, :, n-1] = np.matmul(Vnn[:, :, n-1],
-                                  np.matmul(np.transpose(B),
-                                            np.linalg.inv(Vnn1[:, :, n])))
+        Jn[:, :, n-1] = Vnn[:, :, n-1] @ B.T @ np.linalg.inv(Vnn1[:, :, n])
         xnN[:, :, n-1] = xnn[:, :, n-1] + \
-            np.matmul(Jn[:, :, n-1], xnN[:, :, n]-xnn1[:, :, n])
+            Jn[:, :, n-1] @ (xnN[:, :, n]-xnn1[:, :, n])
         VnN[:, :, n-1] = Vnn[:, :, n-1] + \
-            np.matmul(Jn[:, :, n-1], np.matmul(VnN[:, :, n]-Vnn1[:, :, n],
-                                               np.transpose(Jn[:, :, n-1])))
+            Jn[:, :, n-1] @ (VnN[:, :, n]-Vnn1[:, :, n]) @ Jn[:, :, n-1].T
     # initial state x00 and V00
     # return the smooth estimates of the state at time 0: x0N and V0N
-    J0 = np.matmul(V0, np.matmul(np.transpose(B),
-                                 np.linalg.inv(Vnn1[:, :, 0])))
-    x0N = m0 + np.matmul(J0, (xnN[:, :, 0]-xnn1[:, :, 0]))
-    V0N = V0 + np.matmul(J0, np.matmul(VnN[:, :, 0]-Vnn1[:, :, 0],
-                                       np.transpose(J0)))
+    J0 = V0 @ B.T @ np.linalg.inv(Vnn1[:, :, 0])
+    x0N = m0 + J0 @ (xnN[:, :, 0] - xnn1[:, :, 0])
+    V0N = V0 + J0 @ (VnN[:, :, 0] - Vnn1[:, :, 0]) @ J0.T
     answer = {"xnN": xnN, "VnN": VnN, "Jn": Jn, "x0N": x0N, "V0N": V0N,
               "J0": J0}
     return answer
