4a536fce9857bd5cbbc466bab7f1174365851b88
Joaquin Rapela <joacorapela@gmail.com>
2022-03-30 18:50:01 +0100

    Changed parameter management

--- code/scripts/doSmoothSimulatedTrajectory.py
+++ code/scripts/doSmoothSimulatedTrajectory.py
@@ -6,9 +6,9 @@ import configparser
 import numpy as np
 import pandas as pd
 
+import utils
 sys.path.append("../src")
 import inference
-
 
 def main(argv):
     parser = argparse.ArgumentParser()
@@ -17,12 +17,11 @@ def main(argv):
     parser.add_argument("--simRes_filename_pattern", type=str,
                         default="../../results/{:08d}_simulation.{:s}",
                         help="simulation results filename pattern")
-    parser.add_argument("--smoothing_params_number", type=int,
-                        default=-1,
-                        help="filtering parameters filename number")
-    parser.add_argument("--smoothing_params_filename_pattern", type=str,
-                        default="../../metadata/{:08d}_smoothing.ini",
-                        help="smoothed data filename pattern")
+    parser.add_argument("--filtering_params_filename", type=str,
+                        default="", help="filtering parameters filename")
+    parser.add_argument("--filtering_params_section", type=str,
+                        default="initial_params",
+                        help="section of ini file containing the filtering params")
     parser.add_argument("--results_filenames_pattern", type=str,
                         default="../../results/{:08d}_smoothed.{:s}", 
                         help="results filename pattern")
@@ -30,42 +29,45 @@ def main(argv):
 
     simRes_number = args.simRes_number
     simRes_filename_pattern = args.simRes_filename_pattern
-    smoothing_params_number = args.smoothing_params_number
-    smoothing_params_filename_pattern = args.smoothing_params_filename_pattern
+    filtering_params_filename = args.filtering_params_filename
+    filtering_params_section = args.filtering_params_section
     results_filenames_pattern = args.results_filenames_pattern
 
     simRes_filename = simRes_filename_pattern.format(simRes_number, "npz")
     simRes = np.load(simRes_filename)
 
-    if smoothing_params_number >= 0:
-        smoothing_params_filename = smoothing_params_filename_pattern.format(
-            smoothing_params_number)
+    if len(filtering_params_filename) > 0:
         smoothing_params = configparser.ConfigParser()
-        smoothing_params.read(smoothing_params_filename)
-        pos_x0 = float(smoothing_params["params"]["pos_x0"])
-        pos_y0 = float(smoothing_params["params"]["pos_x0"])
-        vel_x0 = float(smoothing_params["params"]["vel_x0"])
-        vel_y0 = float(smoothing_params["params"]["vel_x0"])
-        ace_x0 = float(smoothing_params["params"]["ace_x0"])
-        ace_y0 = float(smoothing_params["params"]["ace_x0"])
-        sigma_a = float(smoothing_params["params"]["sigma_a"])
-        sigma_x = float(smoothing_params["params"]["sigma_x"])
-        sigma_y = float(smoothing_params["params"]["sigma_y"])
-        V0_diag_value = float(smoothing_params["params"]["V0_diag_value"])
+        smoothing_params.read(filtering_params_filename)
+        pos_x0 = float(smoothing_params[filtering_params_section]["pos_x0"])
+        pos_y0 = float(smoothing_params[filtering_params_section]["pos_y0"])
+        vel_x0 = float(smoothing_params[filtering_params_section]["vel_x0"])
+        vel_y0 = float(smoothing_params[filtering_params_section]["vel_x0"])
+        acc_x0 = float(smoothing_params[filtering_params_section]["acc_x0"])
+        acc_y0 = float(smoothing_params[filtering_params_section]["acc_x0"])
+        sigma_ax = float(smoothing_params[filtering_params_section]["sigma_ax"])
+        sigma_ay = float(smoothing_params[filtering_params_section]["sigma_ay"])
+        sigma_x = float(smoothing_params[filtering_params_section]["sigma_x"])
+        sigma_y = float(smoothing_params[filtering_params_section]["sigma_y"])
+        sqrt_diag_V0_value = float(smoothing_params[filtering_params_section]
+                              ["sqrt_diag_V0_value"])
 
-        m0 = np.array([pos_x0, vel_x0, ace_x0, pos_y0, vel_y0, ace_y0],
+        m0 = np.array([pos_x0, vel_x0, acc_x0, pos_y0, vel_y0, acc_y0],
                       dtype=np.double)
-        V0 = np.diag(np.ones(len(m0))*V0_diag_value)
+        V0 = np.diag(np.ones(len(m0))*sqrt_diag_V0_value**2)
         R = np.diag([sigma_x**2, sigma_y**2])
     else:
-        sigma_a = simRes["sigma_a"]
+        # sigma_ax = simRes["sigma_ax"]
+        # sigma_ay = simRes["sigma_ay"]
+        sigma_ax = simRes["sigma_a"]
+        sigma_ay = simRes["sigma_a"]
         m0 = simRes["m0"]
         V0 = simRes["V0"]
         R = simRes["R"]
 
+    Q = utils.buildQfromQt(Qt=simRes["Qt"], sigma_ax=sigma_ax, sigma_ay=sigma_ay)
     filterRes = inference.filterLDS_SS_withMissingValues(
-        y=simRes["y"], B=simRes["B"], Q=sigma_a**2*simRes["Qt"],
-        m0=m0, V0=V0, Z=simRes["Z"], R=R)
+        y=simRes["y"], B=simRes["B"], Q=Q, m0=m0, V0=V0, Z=simRes["Z"], R=R)
     smoothRes = inference.smoothLDS_SS(B=simRes["B"],
                                        xnn=filterRes["xnn"],
                                        Vnn=filterRes["Vnn"],
@@ -98,13 +100,15 @@ def main(argv):
         res_number, "csv")
 
     df.to_csv(smoothed_data_filename)
+    print("Saved results to {:s}".format(smoothed_data_filename))
     smoothed_metadata = configparser.ConfigParser()
     smoothed_metadata["params"] = {"simRes_number": simRes_number,
-                                   "smoothing_params_number":
-                                   smoothing_params_number}
+                                   "filtering_params_filename":
+                                   filtering_params_filename,
+                                   "filtering_params_section":
+                                   filtering_params_section}
     with open(smoothed_metadata_filename, "w") as f:
         smoothed_metadata.write(f)
-
     import pdb; pdb.set_trace()
 
 
