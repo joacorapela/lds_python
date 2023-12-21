cc7989bbad62408fa5757ec4938b224dc6ecda60
Joaquin Rapela <joacorapela@gmail.com>
2023-12-21 18:56:08 +0000

    Added getLDSmatricesForTracking

--- src/lds/tracking/utils.py
+++ src/lds/tracking/utils.py
@@ -3,6 +3,36 @@ import numpy as np
 import torch
 
 
+def getLDSmatricesForTracking(dt, sigma_a, sigma_x, sigma_y):
+    # Taken from the book
+    # barShalomEtAl01-estimationWithApplicationToTrackingAndNavigation.pdf
+    # section 6.3.3
+
+    # Eq. 6.3.3-2
+    B = np.array([[1, dt, .5*dt**2, 0, 0, 0],
+                   [0, 1, dt, 0, 0, 0],
+                   [0, 0, 1, 0, 0, 0],
+                   [0, 0, 0, 1, dt, .5*dt**2],
+                   [0, 0, 0, 0, 1, dt],
+                   [0, 0, 0, 0, 0, 1]],
+                  dtype=np.double)
+    Z = np.array([[1, 0, 0, 0, 0, 0],
+                   [0, 0, 0, 1, 0, 0]],
+                  dtype=np.double)
+    # Eq. 6.3.3-4
+    Qt = np.array([[dt**4/4, dt**3/2, dt**2/2, 0, 0, 0],
+                   [dt**3/2, dt**2,   dt,      0, 0, 0],
+                   [dt**2/2, dt,      1,       0, 0, 0],
+                   [0, 0, 0, dt**4/4, dt**3/2, dt**2/2],
+                   [0, 0, 0, dt**3/2, dt**2,   dt],
+                   [0, 0, 0, dt**2/2, dt,      1]],
+                  dtype=np.double)
+    R = np.diag([sigma_x**2, sigma_y**2])
+    Q = buildQfromQt_np(Qt=Qt, sigma_ax=sigma_a, sigma_ay=sigma_a)
+
+    return B, Q, Z, R
+
+
 def buildQfromQt_np(Qt, sigma_ax, sigma_ay):
     Q = np.zeros_like(Qt)
     lower_slice = slice(0, int(Q.shape[0]/2))
