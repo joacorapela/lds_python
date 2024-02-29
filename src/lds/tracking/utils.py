f5c427af8de77a6c9b78f7d00b656b34ec3c0d43
Joaquin Rapela <joacorapela@gmail.com>
2024-02-29 14:09:11 +0000

    Renamed Qt to Qe

--- src/lds/tracking/utils.py
+++ src/lds/tracking/utils.py
@@ -20,7 +20,7 @@ def getLDSmatricesForTracking(dt, sigma_a, sigma_x, sigma_y):
                    [0, 0, 0, 1, 0, 0]],
                   dtype=np.double)
     # Eq. 6.3.3-4
-    Qt = np.array([[dt**4/4, dt**3/2, dt**2/2, 0, 0, 0],
+    Qe = np.array([[dt**4/4, dt**3/2, dt**2/2, 0, 0, 0],
                    [dt**3/2, dt**2,   dt,      0, 0, 0],
                    [dt**2/2, dt,      1,       0, 0, 0],
                    [0, 0, 0, dt**4/4, dt**3/2, dt**2/2],
@@ -28,24 +28,24 @@ def getLDSmatricesForTracking(dt, sigma_a, sigma_x, sigma_y):
                    [0, 0, 0, dt**2/2, dt,      1]],
                   dtype=np.double)
     R = np.diag([sigma_x**2, sigma_y**2])
-    Q = buildQfromQt_np(Qt=Qt, sigma_ax=sigma_a, sigma_ay=sigma_a)
+    Q = buildQfromQe_np(Qe=Qe, sigma_ax=sigma_a, sigma_ay=sigma_a)
 
-    return B, Q, Z, R, Qt
+    return B, Q, Z, R, Qe
 
 
-def buildQfromQt_np(Qt, sigma_ax, sigma_ay):
-    Q = np.zeros_like(Qt)
+def buildQfromQe_np(Qe, sigma_ax, sigma_ay):
+    Q = np.zeros_like(Qe)
     lower_slice = slice(0, int(Q.shape[0]/2))
     upper_slice = slice(int(Q.shape[0]/2), Q.shape[0])
-    Q[lower_slice, lower_slice] = sigma_ax**2*Qt[lower_slice, lower_slice]
-    Q[upper_slice, upper_slice] = sigma_ay**2*Qt[upper_slice, upper_slice]
+    Q[lower_slice, lower_slice] = sigma_ax**2*Qe[lower_slice, lower_slice]
+    Q[upper_slice, upper_slice] = sigma_ay**2*Qe[upper_slice, upper_slice]
     return Q
 
 
-def buildQfromQt_torch(Qt, sigma_ax, sigma_ay):
-    Q = torch.zeros_like(Qt)
+def buildQfromQe_torch(Qe, sigma_ax, sigma_ay):
+    Q = torch.zeros_like(Qe)
     lower_slice = slice(0, int(Q.shape[0]/2))
     upper_slice = slice(int(Q.shape[0]/2), Q.shape[0])
-    Q[lower_slice, lower_slice] = sigma_ax**2*Qt[lower_slice, lower_slice]
-    Q[upper_slice, upper_slice] = sigma_ay**2*Qt[upper_slice, upper_slice]
+    Q[lower_slice, lower_slice] = sigma_ax**2*Qe[lower_slice, lower_slice]
+    Q[upper_slice, upper_slice] = sigma_ay**2*Qe[upper_slice, upper_slice]
     return Q
